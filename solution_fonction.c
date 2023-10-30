c0f4ffcf8ddb7a1e9824a907a1ff3d0d18194322
Peniala <peniala.ranaivo@gmail.com>
2023-10-30 10:41:15 +0300

    Ensemble des fonctions utilisées pour calculer la solution du fonction

--- solution_fonction.c
+++ solution_fonction.c
@@ -6,22 +6,24 @@ double f(double x){
 }
 
 // Dichotomie
-// si fabs(f(a)-f(b)) > 0.00001 fait dichotomie sinon pas de solution
+// si f(a)*f(b) < 0 fait dichotomie sinon pas de solution
 double dichotomie(double a,double b){
 	double a1 = 0.0;
-	if(f(a)*f(b) < 0){
-		while(fabs(f(a)-f(b)) > 0.00001){
-			a1 = (a+b)/2;
-			if(f(a)*f(a1) < 0){
-				b = a1;
-			}
-			else{
-				a = a1;
-			}
+	/// if(f(a)*f(b) < 0){
+	while(fabs(f(a)-f(b)) > 0.00001){
+		a1 = (a+b)/2;
+		printf("<tr>\n");
+		printf("<td>%f</td>\n",a);
+		printf("<td>%f</td>\n",b);
+		printf("<td>%f</td>\n",a1);
+		printf("<td>%f</td>\n",f(a1));
+		printf("</tr>\n");
+		if(f(a)*f(a1) <= 0){
+			b = a1;
 		}
-	}
-	else{
-		printf("Pas de solution");
+		else{
+			a = a1;
+		}
 	}
 	return a1;
 }
@@ -38,16 +40,24 @@ double point_Secante(double a, double b){
 	return (-b/a);
 }
 double solution_secante(double a, double b){
-	double i1 = a, i2 = b;
-	if(f(b) < f(a)){
-		i1 = b;
-		i2 = a;
-	}
-	while(fabs(f(i1)) > 0.00001){
- 		double* c = coeff(i1,f(i1),i2,f(i2));
-		i1 = point_Secante(c[0],c[1]);
+	// double i1 = a, i2 = b;
+	// if(f(b) < 0){
+	// 	i1 = b;
+	// 	i2 = a;
+	// }
+	double tmp = 0.0;
+	while(fabs(f(b)) > 0.00001 && a!=b){
+ 		double* c = coeff(a,f(a),b,f(b));
+ 		tmp = b;
+		b = point_Secante(c[0],c[1]);
+		printf("<tr>\n");
+		printf("<td>%f</td>\n",a);
+		printf("<td>%f</td>\n",tmp);
+		printf("<td>%f</td>\n",b);
+		printf("<td>%f</td>\n",f(b));
+		printf("</tr>\n");
  	}
-	return i1;
+	return b;
 }
 
 // Méthode tangente
@@ -55,14 +65,98 @@ double solution_secante(double a, double b){
 
 /// equation de la tangente f'(xo)(x - xo) + f(xo)
 double derive_au_point(double xo){
-	return -(1/xo);
+	return -1/xo;
 }
 double point_tangente(double xo){
 	return (-f(xo)/derive_au_point(xo)) + xo;
 }
 double solution_tangente(double a){
+	double tmp = 0.0;
 	while(fabs(f(a)) > 0.00001){
+		tmp = a;
 		a = point_tangente(a);
+		printf("<tr>\n");
+		printf("<td>%f</td>\n",tmp);
+		printf("<td>%f</td>\n",a);
+		printf("<td>%f</td>\n",f(a));
+		printf("</tr>\n");
 	}
 	return a;
 }
+
+/// Tableau
+void tableIteration(double a,double b,char* method){
+	if(f(a)*f(b) < 0){
+		if(strcmp(method,"dicho")==0){
+			printf("<table>\n");
+			printf("<tr id=\"entete\">\n");
+			printf("<td>a</td>\n");
+			printf("<td>b</td>\n");
+			printf("<td>a1 : between a,b</td>\n");
+			printf("<td>f(a1)</td>\n");
+			printf("</tr>\n");
+
+			dichotomie(a,b);
+
+			printf("</table>\n");
+		}
+		else if(strcmp(method,"newton")==0){
+			printf("<table>\n");
+			printf("<tr id=\"entete\">\n");
+			printf("<td>a</td>\n");
+			printf("<td>Intersection of T(a)</td>\n");
+			printf("<td>f(t)</td>\n");
+			printf("</tr>\n");
+
+			solution_tangente(a);
+
+			printf("</table>\n");
+		}
+		else if(strcmp(method,"descartes")==0){
+			printf("<table>\n");
+			printf("<tr id=\"entete\">\n");
+			printf("<td>a</td>\n");
+			printf("<td>b</td>\n");
+			printf("<td>Intersection of (ab)</td>\n");
+			printf("<td>f(d)</td>\n");
+			printf("</tr>\n");
+
+			solution_secante(a,b);
+
+			printf("</table>\n");
+		}
+	}
+	else {
+		printf("<h1 id=\"pasSolution\">Pas de solution dans l'intervalle [ %lf ; %lf ]</h1>\n",a,b);
+	}
+}
+
+/// compter le nombre d'iteration
+// int countApproximation(){
+// 	FILE* file = fopen("solution.txt","r");
+// 	int c = 0;
+// 	char ligne[300];
+
+// 	if(file == NULL){
+// 		exit(1);
+// 	}
+
+// 	while(fgets(ligne,300,file)!=NULL){
+// 		c++;
+// 	}
+
+// 	fclose(file);
+// 	return c;
+// }
+
+
+// void tableIteration(char* method){
+// 	FILE* file = fopen("solution.txt","r");
+// 	char ligne[300];
+
+// 	if(file == NULL){
+// 		exit(1);
+// 	}
+// 	while(fgets)
+// 	fclose(file);
+// }
